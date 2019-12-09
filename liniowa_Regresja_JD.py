58241c3466c77be68f32d3b7cbcee6b82225f557
jaroslawdrzaszcz <32591153+jaroslawdrzaszcz@users.noreply.github.com>
2019-12-09 19:08:00 +0100

    Shuffle dataset + try HberRegressor model

--- liniowa_Regresja_JD.py
+++ liniowa_Regresja_JD.py
@@ -3,14 +3,19 @@
 # drinking time [hour]. Sobriety  is determined subjectively by the respondents.                                       #
 # Created by Jarosław Drząszcz(s16136).                                                                                #
 ########################################################################################################################
+from sklearn.utils import shuffle
+
 import numpy as np
+from sklearn import linear_model
 from sklearn.svm import SVR
 
 if __name__ == '__main__':
     # Load Statistics of sobriety data
     plik_wejsciowy = "statistics_of_sobriety.txt"
     data = np.loadtxt(plik_wejsciowy, delimiter=',')
-    X, y = data[:, :-1], data[:, -1]
+
+    # Shuffle the data
+    X, y = shuffle(data[:, :-1], data[:, -1], random_state=7)
     print(X)
     print(y)
 
@@ -22,10 +27,11 @@ if __name__ == '__main__':
     X_test, y_test = X[num_traing:], y[num_traing:]
 
     # Create Support Vector Regression model
+    # regressor = linear_model.HuberRegressor()
     regressor = SVR(kernel='linear')
     # Train Support Vector Regressor
     regressor.fit(X_tran, y_tran)
 
     # Test the regressor on test datapoint
-    test_data = [100, 1, 100, 200, 1]
+    test_data = [80, 1, 35, 300, 2]
     print("\nPredicted value:", regressor.predict([test_data]))
